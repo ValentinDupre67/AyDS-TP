331770e3fa3640122ce8df23d5d68b18df368d45
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-06-24 03:47:42 -0300

    2

--- src/main/java/ayds/apolo/songinfo/home/model/HomeModelInjector.kt
+++ src/main/java/ayds/apolo/songinfo/home/model/HomeModelInjector.kt
@@ -3,6 +3,9 @@ package ayds.apolo.songinfo.home.model
 import ayds.apolo.songinfo.home.model.entities.SpotifySong
 import ayds.apolo.songinfo.home.model.repository.SongRepository
 import ayds.apolo.songinfo.home.model.repository.SongRepositoryImpl
+import ayds.apolo.songinfo.home.model.repository.broker.SongBroker
+import ayds.apolo.songinfo.home.model.repository.broker.SongBrokerImpl
+import ayds.apolo.songinfo.home.model.repository.broker.WikipediaProxy
 import ayds.apolo.songinfo.home.model.repository.external.spotify.SpotifyModule
 import ayds.apolo.songinfo.home.model.repository.local.Cache.Cache
 import ayds.apolo.songinfo.home.model.repository.local.Cache.CacheImpl
@@ -16,12 +19,15 @@ object HomeModelInjector {
   internal val internalSpotifiyDataBase = SpotifySqlDBImpl(
     SpotifySqlQueriesImpl(), ResultSetToSpotifySongMapperImpl()
   )
+
   val spotifyTrackService = SpotifyModule.spotifyTrackService
+
+  val broker : SongBroker = SongBrokerImpl(spotifyTrackService, WikipediaProxy())
 
   val theCache = mutableMapOf<String, SpotifySong>()
   val internalCache : Cache = CacheImpl(theCache)
 
-  private val repository: SongRepository = SongRepositoryImpl(internalSpotifiyDataBase, spotifyTrackService, internalCache)
+  private val repository: SongRepository = SongRepositoryImpl(internalSpotifiyDataBase, broker, internalCache)
 
   val homeModel: HomeModel = HomeModelImpl(repository)
 }
